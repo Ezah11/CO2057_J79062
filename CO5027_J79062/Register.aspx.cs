44fd44eea12dacfe86330fa95cd4a32d887076c6
Khadizah Yusop <2484@laksamanacollege.edu.bn>
2021-05-05 05:29:06 +0800

    Database updated

--- CO5027_J79062/Register.aspx.cs
+++ CO5027_J79062/Register.aspx.cs
@@ -20,16 +20,23 @@ namespace CO5027_J79062
         {
             //create a dbcontext that specified the connection string
             var identityDbContext = new IdentityDbContext("IdentityConnectionString");
+            //create role store and role manager
+            var roleStore = new RoleStore<IdentityRole>(identityDbContext);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
             //create user store and user manager
             var userStore = new UserStore<IdentityUser>(identityDbContext);
             var manager = new UserManager<IdentityUser>(userStore);
             //create user
             var user = new IdentityUser() { UserName = txtRegEmail.Text, Email = txtRegEmail.Text };
             IdentityResult result = manager.Create(user, txtRegPassword.Text);
+            IdentityRole adminRole = new IdentityRole("admin");
+            roleManager.Create(adminRole);
             if (result.Succeeded)
             {
+                manager.AddToRole(user.Id, "admin");
+                manager.Update(user);
                 litRegisterError.Text = "Registration succesful.";
-            }
+            }  
             else
             {
                 litRegisterError.Text = "An error has occurred: " + result.Errors.FirstOrDefault();
